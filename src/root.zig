712e9303809a8cd54cabde267220732df40d9e20
feniljain <fkjainco@gmail.com>
2025-09-08 23:58:31 +0530

    feat: api structure

--- src/root.zig
+++ src/root.zig
@@ -1,23 +1,60 @@
 //! By convention, root.zig is the root source file when making a library.
-const std = @import("std");
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+// API: https://riak.com/assets/bitcask-intro.pdf
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+const OpenOptions = struct {
+    /// if this process is going to be a writer
+    /// and not just a reader
+    read_write: bool,
+    /// if this writer would prefer to sync the
+    /// write file after every write operation
+    sync_on_put: bool,
+};
 
-    try stdout.flush(); // Don't forget to flush!
-}
+const BitCask = struct {
+    // TODO: How to make sure only one process
+    // opens it with read_write? 
+    // OS level dir locks?
+    // multiple readers are fine.
+    pub fn open(dir_name: []const u8) !BitCask {
+        _ = dir_name;
+        return BitCask {};
+    }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+    pub fn get(self: *BitCask, key: []const u8) ![]const u8 {
+        _ = self;
+        _ = key;
+        return "";
+    }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
+    pub fn put(self: *BitCask, key: []const u8, value: []const u8) !void {
+        _ = self;
+        _ = key;
+        _ = value;
+    }
+
+    pub fn delete(self: *BitCask, key: []const u8) !void {
+        _ = self;
+        _ = key;
+    }
+
+    pub fn list_keys(self: *BitCask) ![][]const u8 {
+        _ = self;
+        return [1][1]u8{[_]u8{ 'a' }};
+    }
+
+    // pub fn fold(self: *BitCask) !void {
+    // }
+
+    pub fn merge(self: *BitCask) !void {
+        _ = self;
+    }
+
+    pub fn sync(self: *BitCask) !void {
+        _ = self;
+    }
+
+    pub fn close(self: *BitCask) !void {
+        _ = self;
+    }
+};

131c3dd77983493d1cfa9da5176deb92490c7d4a
feniljain <fkjainco@gmail.com>
2025-09-27 21:57:51 +0530

    wip: build_keydir

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,6 @@
 // API: https://riak.com/assets/bitcask-intro.pdf
 
+// TODO: use this
 const OpenOptions = struct {
     /// if this process is going to be a writer
     /// and not just a reader
@@ -43,13 +44,80 @@ pub const BitCask = struct {
         const file_name = try std.fmt.allocPrint(smp_allocator, "data-{d}", .{1});
         defer smp_allocator.free(file_name);
 
-        // TODO: mark truncate as false, and exclusive as true, how to make
-        // file access as exclusive?
-        const file = try dir.createFile(file_name, .{ .read = true, .truncate = true, .exclusive = false });
+        var new_file_created = false;
 
-        try file.seekFromEnd(0);
+        const file = dir.openFile(
+            file_name,
+            .{ .mode = fs.File.OpenMode.read_write }) catch blk: {
+                // TODO: should we make file access as exclusive?
+                const file = try dir.createFile(file_name, .{ .read = true, .truncate = true, .exclusive = false });
+                new_file_created = true;
+                break :blk file;
+            };
 
-        return BitCask{ .file = file };
+
+        var bc = BitCask{ .file = file };
+
+        if(!new_file_created) {
+            try bc.build_keydir();
+        }
+
+        return bc;
+    }
+
+    pub fn build_keydir(self: *BitCask) !void {
+        const file_len = try self.file.getEndPos();
+
+        var curr_pos: u64 = 0;
+        try self.file.seekTo(curr_pos);
+        const lenbyts = try self.allocator.alloc(u8, 4);
+
+        std.debug.print("DEBUG::curr_pos::{d} - file_len::{d}\n", .{curr_pos, file_len});
+
+        // TODO
+        const is_file_corrupted: bool = false;
+
+        // FIX: set is_file_corrupted instead of
+        // returning from method
+        while(curr_pos < file_len) {
+            _ = try self.file.read(lenbyts);
+            const key_len = mem.readInt(u32, lenbyts[0..4], Endian.big);
+            curr_pos += 4;
+
+            std.debug.print("DEBUG::key_len::{d}\n", .{key_len});
+
+            const key = try self.allocator.alloc(u8, key_len);
+            _ = try self.file.read(key);
+            curr_pos += key_len;
+
+            std.debug.print("DEBUG::key::{s}\n", .{key});
+
+            _ = try self.file.read(lenbyts);
+            const value_len = mem.readInt(u32, lenbyts[0..4], Endian.big);
+            curr_pos += 4;
+
+            std.debug.print("DEBUG::value_len::{d}\n", .{value_len});
+
+            // don't read tombstone
+            if(value_len != 0) {
+                // insert into keydir
+                try self.keydir.put(key, .{ .value_offset = curr_pos, .value_len = value_len });
+
+                // skip reading value
+                curr_pos += value_len;
+                _ = try self.file.seekTo(curr_pos);
+            }
+        }
+
+        std.debug.print("DEBUG::curr_pos::{d} - file_len::{d}\n", .{curr_pos, file_len});
+        assert(curr_pos <= file_len);
+        if(is_file_corrupted or curr_pos != file_len) {
+            // if current_position is not perfectly as file_len
+            // that means corruption has happened,
+            // truncate remaining file
+            try self.file.setEndPos(curr_pos);
+            std.debug.print("DEBUG::corrupted file, truncating", .{});
+        }
     }
 
     pub fn get(self: *BitCask, key: []const u8) ![]u8 {
@@ -201,16 +269,67 @@ test "test_bitcask_delete" {
     try bitcask.close();
 }
 
+test "test_bitcask_build_keydir" {
+    var bitcask = try BitCask.open("./data");
+
+    const key_1: string = "melody";
+    const value_1: string = "itni choclaty kyun hai";
+
+    try bitcask.put(key_1, value_1);
+
+    const received_value_1 = try bitcask.get(key_1);
+    try expect(std.mem.eql(u8, value_1, received_value_1));
+
+    try bitcask.close();
+
+    var bitcask_1 = try BitCask.open("./data");
+
+    const received_value_2 = try bitcask_1.get(key_1);
+    try expect(std.mem.eql(u8, value_1, received_value_2));
+
+    try bitcask_1.close();
+}
+
+// // TODO(test): corrupt file
+// test "test_bitcask_build_keydir_corrupt_file" {
+//     var bitcask = try BitCask.open("./data");
+//
+//     const key_1: string = "melody";
+//     const value_1: string = "itni choclaty kyun hai";
+//
+//     try bitcask.put(key_1, value_1);
+//
+//     const received_value_1 = try bitcask.get(key_1);
+//     try expect(std.mem.eql(u8, value_1, received_value_1));
+//
+//     // corrupt the file by writing just key_len
+//     const lenbyts = try bitcask.allocator.alloc(u8, 4);
+//     mem.writeInt(u32, lenbyts[0..4], 10, Endian.big);
+//     try bitcask.file.writeAll(lenbyts);
+//
+//     try bitcask.close();
+//
+//     var bitcask_1 = try BitCask.open("./data");
+//
+//     const received_value_2 = try bitcask_1.get(key_1);
+//     try expect(std.mem.eql(u8, value_1, received_value_2));
+//
+//
+//     try bitcask_1.close();
+// }
+
 // TODO:
 // - build_keydir
 // - list_keys
 // - merge
+// - iterator
 
 // https://github.com/oven-sh/bun/blob/3b7d1f7be28ecafabb8828d2d53f77898f45312f/src/open.zig#L437
 const string = []const u8;
 const smp_allocator = std.heap.smp_allocator;
 
 const std = @import("std");
+const assert = std.debug.assert;
 const HashMap = std.array_hash_map.StringArrayHashMap;
 const Endian = std.builtin.Endian;
 const heap = std.heap;
